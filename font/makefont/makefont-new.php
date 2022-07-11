9cb3b5255ccecb677288a16319bc9c1c428e7ad1
Hendrik Diel <hendrik.diel@bsi-software.com>
2022-07-11 11:01:37 +0200

    apply rector php53 profile

--- font/makefont/makefont-new.php
+++ font/makefont/makefont-new.php
@@ -81,7 +81,7 @@ function write_dict($dict, $fname) {
 function ReadMap($enc)
 {
 	//Read a map file
-	$file=dirname(__FILE__).'/'.strtolower($enc).'.map';
+	$file=__DIR__.'/'.strtolower($enc).'.map';
 	$a=file($file);
 	if(empty($a))
 		die('<B>Error:</B> encoding not found: '.$enc);
